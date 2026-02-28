a1f9d35874a3cb65dffd17ac7d2a5c697873de13
hashtafak <jamestran201@gmail.com>
2026-02-28 14:30:11 +0700

    refactor ISMoveablesAction patches

--- manipulation_authority_framework/42/media/lua/shared/manipulation_authority_framework/patches/is_moveables_action_patch.lua
+++ manipulation_authority_framework/42/media/lua/shared/manipulation_authority_framework/patches/is_moveables_action_patch.lua
@@ -1,43 +1,72 @@
 local patches = {}
 
 function patches.clientSidePatch()
+    -- Add isValid hook for visual phase checking during action
     local original_ISMoveablesAction_isValidObject = ISMoveablesAction.isValidObject
     function ISMoveablesAction:isValidObject()
         local MAF = _G.ManipulationAuthorityFramework
-        if MAF and MAF.config.ISMoveablesActionProtection then
+        local MAFV = _G.ManipulationAuthorityFrameworkVisual
+        if MAF and MAF.config.ISMoveablesActionProtection and MAFV then
+            ---@cast self ISMoveablesAction
             local object = self.object
-            if object and object:getModData().isShop then
-                -- Block Pickup and Scrap as a direct detour (not event-based for performance)
-                if self.mode == "pickup" or self.mode == "scrap" then
+            local mode = self.mode
+            ---@diagnostic disable-next-line: unnecessary-if
+            if object and (mode == "pickup" or mode == "scrap") then
+                local ctx = MAFV:getVisualContext(
+                    "Moveables",
+                    object,
+                    self.character,
+                    object:getSquare(),
+                    mode
+                )
+                ctx.action = self
+                MAFV:processAction(ctx)
+                if ctx.flags.rejected then
                     return false
                 end
             end
         end
         return original_ISMoveablesAction_isValidObject(self)
     end
+
+    -- So we should regis the hook from both client and server
+    -- local original_new = ISMoveablesAction.new
+    -- function ISMoveablesAction:new(character, square, mode, origSpriteName, object, direction, item, moveCursor)
+    --     local o = original_new(self, character, square, mode, origSpriteName, object, direction, item, moveCursor)
+    --     local MAF = _G.ManipulationAuthorityFramework
+    --     if MAF and MAF.config.ISMoveablesActionProtection then
+    --         ---@diagnostic disable-next-line: unnecessary-if
+    --         if o.object and (o.mode == "pickup" or o.mode == "scrap") then
+    --             local ctx = MAF:createContext("Moveables", o, o.object, character)
+    --             -- Fire pre-action phase (registration/instance capture)
+    --             MAF:processAction("pre", ctx)
+    --             -- Note: if we needed to override maxTime, we would set o.maxTime here on creation
+    --             -- rather than inside getDuration().
+    --         end
+    --     end
+    --     return o
+    -- end
 end
 
 function patches.serverSidePatch()
-    local original_ISMoveablesAction_getDuration = ISMoveablesAction.getDuration
-    function ISMoveablesAction:getDuration()
-        local MAF = _G.ManipulationAuthorityFramework
-        if MAF and MAF.config.ISMoveablesActionProtection then
-            local object = self.object
-            if object and (self.mode == "pickup" or self.mode == "scrap") then
-                local ctx = MAF:createContext("Moveables", self, object, self.character)
-                -- Fire pre-action phase (registration/instance capture)
-                MAF:processAction("pre", ctx)
-            end
-        end
-        return original_ISMoveablesAction_getDuration(self)
-    end
+    -- [Performance Intel]
+    -- ISMoveablesAction:getDuration() is called many times (not just one time).
+    -- To avoid performance issues and redundant calls, we don't process the 'pre' phase
+    -- or set maxTime inside getDuration. The rule is to fire the 'pre' phase on creation and use :stop()
+    -- to cancel the action, rather than manipulating getDuration().
+    -- local original_ISMoveablesAction_getDuration = ISMoveablesAction.getDuration
+    -- function ISMoveablesAction:getDuration()
+    --     -- Ensure getDuration hooks only return a number.
+    --     return original_ISMoveablesAction_getDuration(self)
+    -- end
 
     local original_ISMoveablesAction_complete = ISMoveablesAction.complete
     function ISMoveablesAction:complete()
         local MAF = _G.ManipulationAuthorityFramework
         if MAF and MAF.config.ISMoveablesActionProtection then
             local object = self.object
-            if object and (self.mode == "pickup" or self.mode == "scrap") then
+            local mode = self.mode
+            if object and (mode == "pickup" or mode == "scrap") then
                 local ctx = MAF:createContext("Moveables", self, object, self.character)
                 MAF:processAction("validate", ctx)
 
